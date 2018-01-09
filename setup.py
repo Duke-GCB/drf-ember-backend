5623150797fd05699b262e0369a6a8c4905f3d56
John Bradley <johnbradley2008@gmail.com>
2018-01-09 14:09:50 -0500

    bump to version 1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='drf-ember-backend',
-    version='1.0',
+    version='1.1',
     packages=['drf_ember_backend'],
     install_requires=[
         'Django',
